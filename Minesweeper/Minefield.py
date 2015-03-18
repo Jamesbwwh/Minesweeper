aba09c3ccd93fced8eb05e898db36ca32f66c7b4
James Woo <Jamesbrianwoo@gmail.com>
2015-03-19 02:37:46 +0800

    Update: UI Changes, Button Press Logic

--- Minesweeper/Minefield.py
+++ Minesweeper/Minefield.py
@@ -1,24 +1,24 @@
 def printMineField(difficulty):
     #easy difficulty
     if (difficulty == 1):
-        minefield = [[0 for x in range(15)] for x in range(15)]
+        minefield = [[0] * 15 for i in xrange(15)]
         return minefield
         
     #medium difficulty
     if (difficulty == 2):
-        minefield = [[0 for x in range(50)] for x in range(50)]
+        minefield = [[0] * 50 for i in xrange(50)]
         return minefield
 
     #hard difficulty
     if (difficulty == 3):
-        minefield = [[0 for x in range(150)] for x in range(150)]
+        minefield = [[0] * 150 for i in xrange(150)]
         return minefield
 
     #custom difficulty
     if (difficulty == 0):
         width = input("Enter the width : ")
         height = input("Enter the height : ")
-        minefield = [[0 for x in range(width)] for x in range(height)]
+        minefield = [[0] * width for i in xrange(height)]
         return minefield
 
 def displayTestMineField(minemap, limiter):
--- Minesweeper/Minesweeper.py
+++ Minesweeper/Minesweeper.py
@@ -1,8 +1,10 @@
 import gtk
+import copy
 import random
 import Minefield
 
 mineField = []
+frameField = []
 
 def generate(difficulty):
     global mineField
@@ -10,7 +12,7 @@ def generate(difficulty):
     height = len(mineField)
     width = len(mineField[0])
     ranges = width * height
-    mines = random.randint(ranges // 4, ranges // 3)
+    mines = random.randint(ranges // 8, ranges // 7)
     ranges -= 1
     for mine in range(mines):
         while(True):
@@ -45,39 +47,124 @@ def generate(difficulty):
                             mineField[x + 1][y + 1] += 1
                 break
 
-class Minesweeper:
+def exploreMineless(world,i,j):
+    N = len(world)
+    M = len(world[0])
+    if i < 0 or j < 0 or i >= N or j >= M:
+        return
+    if world[i][j] is 0:
+        world[i][j] = 9
+        exploreMineless(world,i - 1,j) #up
+        exploreMineless(world,i + 1,j) #down
+        exploreMineless(world,i,j - 1) #left
+        exploreMineless(world,i,j + 1) #right
+    label = gtk.Label(str(mineField[i][j]))
+    label.set_size_request(20, 20)
+    frame = frameField[i][j]
+    widget = frame.get_child()
+    frame.remove(widget)
+    frame.add(label)
+    frame.set_shadow_type(gtk.SHADOW_OUT)
+    label.show()
+
+class About:
     def quit_event(self, widget):
         self.window.destroy()
 
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("about.glade")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("aboutMinesweeper")
+        self.window.show_all()
+
+class Options:
+    def quit_event(self, widget):
+        self.window.destroy()
+
+    def ok_event(self, widget):
+        pass
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("options.glade")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("Options")
+        self.window.show_all()
+
+class Statistics:
+    def quit_event(self, widget):
+        self.window.destroy()
+
+    def reset_stats_event(self, widget):
+        pass
+
+    def __init__(self):
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("statistics.glade")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("Statistics")
+        self.window.show_all()
+
+class Minesweeper:
+    def quit_event(self, widget):
+        self.window.destroy()
+        gtk.main_quit()
+
     def new_game_event(self, widget):
-        md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "New Game")
+        md = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, "New Game")
         md.run()
 
     def options_event(self, widget):
-        md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "Options")
+        md = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, "Options")
         md.run()
 
     def statistics_event(self, widget):
-        md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "Statistics")
+        md = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, "Statistics")
         md.run()
 
     def about_event(self, widget):
-        md = gtk.MessageDialog(None, gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO, gtk.BUTTONS_CLOSE, "About")
+        md = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, "About")
         md.run()
 
+    def button_event(self, widget, i, j):
+        #str(mineField[i][j])
+        if mineField[i][j] is 9:
+            md = gtk.MessageDialog(None, gtk.DIALOG_MODAL, gtk.MESSAGE_INFO, gtk.BUTTONS_NONE, "Explosion")
+            md.run()
+        elif mineField[i][j] is 0:
+            map = copy.deepcopy(mineField)
+            exploreMineless(map, i, j)
+            pass
+        else:
+            label = gtk.Label(str(mineField[i][j]))
+            label.set_size_request(20, 20)
+            frame = widget.parent
+            frame.remove(widget)
+            frame.add(label)
+            frame.set_shadow_type(gtk.SHADOW_OUT)
+            label.show()
+
     def __init__(self):
+        global frameField
         self.builder = gtk.Builder()
         self.builder.add_from_file("minesweeper.glade")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("Minesweeper")
         vbox = self.builder.get_object("vBox")
+        frameField = [[None] * len(mineField) for i in xrange(len(mineField[0]))]
         for i in range(len(mineField)):
             hbox = gtk.HBox()
             vbox.pack_start(hbox, False, False, 0)
             for j in range(len(mineField[0])):
-                button = gtk.Button(str(mineField[i][j]))
+                button = gtk.Button()
+                button.connect("clicked", self.button_event, i, j)
                 button.set_size_request(20, 20)
-                hbox.pack_start(button, False, False, 0)
+                frame = gtk.Frame()
+                frame.add(button)
+                frame.set_shadow_type(gtk.SHADOW_NONE)
+                frameField[i][j] = frame
+                hbox.pack_start(frame, False, False, 0)
         self.window.show_all()
 
 generate(1)
